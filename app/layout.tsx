b89b308b44f8a5d42445e1d3d366b39eb60cb12e
n0t <lossany08@gmail.com>
2025-09-11 23:01:33 +0700

    fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,14 +104,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      {/* <Head>
         <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="shortcut icon" href="/favicon-32x32.png" sizes="32x32" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        {/* <link rel="preload" href="/android-chrome-512x512.png" as="image" /> */}
-      </Head>
+        
+      </Head> */}
       <body
         className={`font-sans ${spaceGrotesk.variable} ${dmSans.variable} antialiased`}
       >
